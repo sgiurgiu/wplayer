a8922c7a5926121c5fd77a36c43068752e3c30e7
Sergiu Giurgiu <sgiurgiu11@gmail.com>
2017-02-05 02:24:36 -0500

    added filter to the files list and a feedback message when starting a movie

--- web/js/movies.js
+++ web/js/movies.js
@@ -2,14 +2,17 @@ angular.module('movies', ['files.service','player.service']);
 
 angular.module('movies').component('movies', {
     templateUrl:'movies.html',
-    controller:['$routeParams','$log','$location','Files','Player',  function($routeParams,$log,$location,Files,Player) {
+    controller:['$routeParams','$log','$location','$timeout','Files','Player',  function($routeParams,$log,$location,$timeout,Files,Player) {
         var self = this;
         self.loading = true;
+        self.playStartedSuccess = false;
 	self.files_list = [];
+        self.original_file_list=[];
         Files.list({'path':$routeParams.path},function(data){
             self.loading = false;
             self.cur_dir = data.cur_dir;
             self.files_list = data.files;
+            self.original_file_list = JSON.parse(JSON.stringify(self.files_list));
             $log.log('got files '+data);                
         }, function(error){
             self.loading = false;   
@@ -17,7 +20,16 @@ angular.module('movies').component('movies', {
 
 	self.playFile = function(file) {
             Player.playFile(file);
+            self.playStartedSuccess = true;
+            $timeout(function(){
+                self.playStartedSuccess = false;
+            },5000);
         };
+        self.closeMessage = function(){
+            self.playStartedSuccess = false;
+        };
+        
+        self.searchText = "";
     }
         
     ]
